27d41acbd46973a3319c6e25f2f18de361e9c72f
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-09-12 23:29:49 -0300

    definir componente main

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React!</h1>
+            <ul className="facts-list">
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has wall over 200k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    );
+}
